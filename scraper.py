02deef4e9e7246c7a2b6683f5f82f81b60c8f4d0
f-ptak <s222693@student.uek.krakow.pl>
2022-05-06 12:58:09 +0200

    2022-05-06 FP

--- scraper.py
+++ scraper.py
@@ -2,13 +2,39 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+def get_item(ancestor, selector, attribute=None, return_list=False):
+    '''docstring right here'''
+    try:
+        if return_list:
+            return [item.get_text().strip() for item in ancestor.select_one(selector)]
+        if attribute:
+            return ancestor.select_one(selector)[attribute]
+        return ancestor.select_one(selector).get_text().strip()
+    except (AttributeError, TypeError):
+        return None
+
+selectors = {
+    "opinion_id": ["data-entry-id"],
+    "author": ["span.user-post__author-name"],
+    "recomendation": ["span.user-post__author-recomendation> em.recommended"],
+    "stars": ["span.user-post__score-count"],
+    "content": ["div.user-post__text"],
+    "useful": ["button.vote-yes > span"],
+    "useless": ["button.vote-no > span"],
+    "published": ["span.user-post__published > time:nth-child(1)", "datetime"],
+    "purchased": ["span.user-post__published > time:nth-child(2)", "datetime"],
+    "pros": ["div[class$=positives] ~ div.review-feature__item", None, True],
+    "cons": ["div[class$=negatives] ~ div.review-feature__item", None, True]
+}
+
 product_id = input("Please enter the product ID:\n").replace(" ", "")
 print("Product ID entered. Igniting the Scrape Machine...")
 
-product_url = "https://www.ceneo.pl/" + product_id + "#tab=reviews"
+product_url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
 
 all_opinions = []
-opinion_amount = 0
+opinion_counter = 0
+page_counter = 1
 
 while product_url:
     response = requests.get(product_url)
@@ -16,49 +42,24 @@ while product_url:
     opinions = page.select("div.js_product-review")
 
     for opinion in opinions:
-        opinion_id = opinion["data-entry-id"]
-        author = opinion.select_one("span.user-post__author-name").get_text().strip()
-        try:
-            recommendation = opinion.select_one("span.user-post__author-recomendation> em.recommended").get_text().strip()
-        except AttributeError:
-            recommendation = None
-        stars = opinion.select_one("span.user-post__score-count").get_text().strip()
-        content = opinion.select_one("div.user-post__text").get_text().strip()
-        useful = opinion.select_one("button.vote-yes > span").get_text().strip()
-        useless = opinion.select_one("button.vote-no > span").get_text().strip()
-        published_date = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
-        try:
-            purchased_date = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
-        except TypeError:
-            purchased_date = None
-        pros = opinion.select("div[class$=positives] ~ div.review-feature__item")
-        pros = [item.get_text().strip() for item in pros]
-        cons = opinion.select("div[class$=negatives] ~ div.review-feature__item")
-        cons = [item.get_text().strip() for item in cons]
-
         single_opinion = {
-            "opinion_id": opinion_id,
-            "author": author,
-            "recomendation": recommendation,
-            "stars": stars,
-            "content": content,
-            "useful": useful,
-            "useless": useless,
-            "published": published_date,
-            "purchased": purchased_date,
-            "pros": pros,
-            "cons": cons
-        }
+            key:get_item(opinion, *value)
+                for key, value in selectors.items()
+    }
+        single_opinion["opinion_id"] = opinion["data-entry-id"]
 
         all_opinions.append(single_opinion)
-        opinion_amount += 1
+        opinion_counter += 1
+
+    print(f"Processing page number {page_counter}...")
+    page_counter += 1
 
     try:
         product_url = "https://www.ceneo.pl" + page.select_one("a.pagination__next")["href"]
     except TypeError:
         product_url = None
 
-with open("opinions/" + product_id + ".json", "w", encoding="UTF-8") as jf:
+with open(f"opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
     json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
 
-print(f"Done! Opinions found in the scrapescape: {opinion_amount}.")
+print(f"Done! Opinions found in the scrapescape: {opinion_counter}.")
