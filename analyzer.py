49f108f493335826491899149233c39dbaea5602
f-ptak <s222693@student.uek.krakow.pl>
2022-05-14 23:59:07 +0200

    2022-05-13 FP fix

--- analyzer.py
+++ analyzer.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 
 print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep="\n")
 product_id = input("Please enter the product ID:\n")
+print("Analyzing...")
 
 opinions = pd.read_json(f"opinions/{product_id}.json")
 opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",",".")))
@@ -14,7 +15,7 @@ pros_count = opinions["pros"].map(bool).sum()
 cons_count = opinions["cons"].map(bool).sum()
 average_score = opinions["stars"].mean().round(2)
 
-recommendation = opinions["recomendation"].value_counts(dropna=False).sort_index().reindex(["Nie polecam", "Polecam", None], fill_value=0)
+recommendation = opinions["recommendation"].value_counts(dropna=False).sort_index().reindex(["Nie polecam", "Polecam", None], fill_value=0)
 recommendation.plot.pie(
     label="",
     autopct = lambda p: '{:.1f}%'.format(round(p)) if p > 0 else '',
@@ -27,7 +28,9 @@ plt.savefig(f"plots/{product_id}_recommendations.png")
 plt.close()
 
 stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
-stars.plot.bar()
+stars.plot.bar(
+    color = "coral"
+)
 plt.title("Oceny produktu")
 plt.xlabel("Liczba gwiazdek")
 plt.ylabel("Liczba opinii")
@@ -35,3 +38,5 @@ plt.grid(True, axis="y")
 plt.xticks(rotation=0)
 plt.savefig(f"plots/{product_id}_stars.png")
 plt.close()
+
+print("Done!")
--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ def get_item(ancestor, selector, attribute=None, return_list=False):
     '''docstring right here'''
     try:
         if return_list:
-            return [item.get_text().strip() for item in ancestor.select_one(selector)]
+            return [item.get_text().strip() for item in ancestor.select(selector)]
         if attribute:
             return ancestor.select_one(selector)[attribute]
         return ancestor.select_one(selector).get_text().strip()
@@ -14,9 +14,8 @@ def get_item(ancestor, selector, attribute=None, return_list=False):
         return None
 
 selectors = {
-    "opinion_id": ["data-entry-id"],
     "author": ["span.user-post__author-name"],
-    "recomendation": ["span.user-post__author-recomendation> em.recommended"],
+    "recommendation": ["span.user-post__author-recomendation> em"],
     "stars": ["span.user-post__score-count"],
     "content": ["div.user-post__text"],
     "useful": ["button.vote-yes > span"],
@@ -33,7 +32,6 @@ print("Product ID entered. Igniting the Scrape Machine...")
 product_url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
 
 all_opinions = []
-opinion_counter = 0
 page_counter = 1
 
 while product_url:
@@ -45,11 +43,9 @@ while product_url:
         single_opinion = {
             key:get_item(opinion, *value)
                 for key, value in selectors.items()
-    }
+        }
         single_opinion["opinion_id"] = opinion["data-entry-id"]
-
         all_opinions.append(single_opinion)
-        opinion_counter += 1
 
     print(f"Processing page number {page_counter}...")
     page_counter += 1
@@ -62,4 +58,4 @@ while product_url:
 with open(f"opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
     json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
 
-print(f"Done! Opinions found in the scrapescape: {opinion_counter}.")
+print(f"Done!")
